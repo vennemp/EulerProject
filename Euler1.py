3d620a175449ab37f7d2f6642e775545fe47142f
vennemp <vennemp@gmail.com>
2019-03-23 19:50:56 -0400

    Update Euler1.py

--- Euler1.py
+++ Euler1.py
@@ -1,9 +1,3 @@
 #sum all integers from 1-1000 that are divisible by 3 or 5
 
-i = 0
-sum = 0
-while i < 1001:
-    i+=1
-    if i%3==0 or i%5==0:
-        sum+=i
-print(sum)
+sum([x for x in range(1000) if ((x % 5 == 0) or (x % 3 == 0))])
